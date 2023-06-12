61ddb089d646b4398cfa53f3da868b24a916c83b
shivam-1-byte <105426207+shivam-1-byte@users.noreply.github.com>
2023-06-12 23:52:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function addTask() {
+    var taskInput = document.getElementById("taskInput");
+    var taskList = document.getElementById("taskList");
+    
+    if (taskInput.value === "") {
+        alert("Please enter a task.");
+        return;
+    }
+    
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(taskInput.value));
+    taskList.appendChild(li);
+    
+    taskInput.value = "";
+}
+
+
